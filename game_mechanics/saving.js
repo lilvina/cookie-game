e9533ef8e5663279a9b78e173a24c16b3151c6e1
Breyana Scales <Breyana@anasauce1.local>
2016-12-07 15:41:14 -0800

    updated the clicking and saving files to have new game support and updating on load support

--- game_mechanics/saving.js
+++ game_mechanics/saving.js
@@ -7,6 +7,11 @@ function save() {
   localStorage.setItem("clickMultiplier", JSON.stringify(clickMultiplier));
 }
 
+function newGame() {
+  localStorage.setItem("cookieTotal", JSON.stringify(0));
+  localStorage.setItem("clickMultiplier", JSON.stringify(0));
+}
+
 function load() {
     cookieTotal = JSON.parse(localStorage.getItem("cookieTotal"));
     clickMultiplier = JSON.parse(localStorage.getItem("clickMultiplier"));
@@ -15,4 +20,5 @@ function load() {
 
 
 document.getElementById('savegame').onclick = save;
+document.getElementById('newgame').onclick = newGame;
 document.addEventListener("DOMContentLoaded", load());
