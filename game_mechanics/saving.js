07e44de0711c2348aee3c305463f8a1e97fd26fa
Breyana Scales <Breyana@anasauce1.local>
2016-12-09 09:09:59 -0800

    fixed loading on new browsers part 2

--- game_mechanics/saving.js
+++ game_mechanics/saving.js
@@ -28,25 +28,25 @@ function load() {
     cookieTotal = 0
   } else {
     cookieTotal = JSON.parse(localStorage.getItem("cookieTotal"));
-  }
+  };
 
   if (localStorage.getItem("clickMultiplier") === null) {
     clickMultiplier = 1
   } else {
     clickMultiplier = JSON.parse(localStorage.getItem("clickMultiplier"));
-  }
+  };
 
   if (localStorage.getItem("grandmaTotal") === null) {
     grandmaTotal = 0
   } else {
     grandmaTotal = JSON.parse(localStorage.getItem("grandmaTotal"));
-  }
+  };
 
   if (localStorage.getItem("bakeryTotal") === null) {
     bakeryTotal = 0
   } else {
     bakeryTotal = JSON.parse(localStorage.getItem("bakeryTotal"));
-  }
+  };
 }
 
 window.setInterval(save, 1000)
