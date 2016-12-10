d980336bae01694d2a46157427bb3c4347572248
Breyana Scales <Breyana@anasauce1.local>
2016-12-09 17:22:01 -0800

    added in the farm

--- game_mechanics/clicking.js
+++ game_mechanics/clicking.js
@@ -2,6 +2,8 @@ var cookieTotal = 0
 var clickMultiplier = 1
 var grandmaTotal = 0
 var bakeryTotal = 0
+var cowTotal = 0
+var milkTotal = 0
 
 //const counter = 0
 //on click, increase cookieTotal * clickMultiplier
@@ -71,17 +73,87 @@ function grandmas() {
 
 function farmDiv(){
   var newDiv = document.createElement("div");
-  var newContent = document.createTextNode("Farm node!!!");
+  var newContent = document.createTextNode("Farm Mode Unlocked!");
+  var cowBtn = document.createElement("button");
+  var milkBtn = document.createElement("button");
+  var t = document.createTextNode("Buy Cows");
+  var m = document.createTextNode("Buy Milk");
+  var innerDivCow = document.createElement("div");
+  var innerDivMilk = document.createElement("div");
+  var cowExplanation = document.createTextNode(" 40,000 Cookies for a Cow")
+  var milkExplanation = document.createTextNode(" Each cow eats 10,000 cookies")
+  var cowCount = document.createTextNode("0")
+  var milkCount = document.createTextNode("0")
+  var cowCountDiv = document.createElement("div");
+  var milkCountDiv = document.createElement("div");
+
   newDiv.appendChild(newContent);
+  newDiv.appendChild(innerDivCow);
+  newDiv.appendChild(innerDivMilk);
+
+  cowCountDiv.appendChild(cowCount);
+  milkCountDiv.appendChild(milkCount);
+
+
+  newDiv.style.backgroundImage = "url('farm_bg.jpg')";
+  newDiv.className = "farm-div"
+
+  cowBtn.id = "cow-button"
+  milkBtn.id = "milk-button"
+  cowCountDiv.id = "cow-count"
+  milkCountDiv.id = "milk-count"
+
+
+  cowBtn.appendChild(t)
+  milkBtn.appendChild(m)
+  innerDivCow.appendChild(cowBtn)
+  innerDivCow.appendChild(cowExplanation)
+  innerDivCow.appendChild(cowCountDiv)
+
+  innerDivMilk.appendChild(milkBtn)
+  innerDivMilk.appendChild(milkExplanation)
+  innerDivMilk.appendChild(milkCountDiv)
+  // newDiv.innerHTML =
   var currentDiv = document.getElementById("item");
-  document.body.insertBefore(newDiv, currentDiv);
+  currentDiv.parentNode.insertBefore(newDiv, currentDiv);
+  document.getElementById('cow-button').onclick = cowPurchase;
+  document.getElementById('milk-button').onclick = milkCows;
+}
+var checkFarmFunctionHasRan = 0
+function farmCheck() {
+  if(bakeryTotal >= 2 && checkFarmFunctionHasRan < 1){
+    farmDiv();
+    checkFarmFunctionHasRan = 1
+  }
 }
 
-if(bakeryTotal === 2){
-  farmDiv();
+function cowPurchase() {
+  if (cookieTotal < 40000) {
+    return cowTotal
+  } else {
+    cookieTotal -= 40000;
+    cowTotal += 1
+    document.getElementById("cow-count").innerHTML = cowTotal
+    document.getElementById("cookie_total").innerHTML = "Total Cookies: " + cookieTotal;
+    document.getElementById("cookie_total2").innerHTML = "Total Cookies: " + cookieTotal
+  }
+}
+
+function milkCows() {
+  if (cookieTotal < (10000 * cowTotal)) {
+    return milkTotal
+  } else {
+    cookieTotal -= 10000 * cowTotal
+    milkTotal += cowTotal
+    document.getElementById("milk-count").innerHTML = milkTotal
+    document.getElementById("cookie_total").innerHTML = "Total Cookies: " + cookieTotal;
+    document.getElementById("cookie_total2").innerHTML = "Total Cookies: " + cookieTotal
+  }
 }
 
 window.setInterval(grandmas, 1000 )
+window.setInterval(farmCheck, 1000 )
+
 document.getElementById('savecookie').onclick = cookieClick;
 document.getElementById('oven').onclick = oven;
 document.getElementById('grandma').onclick = grandmaPurchase;
--- game_mechanics/saving.js
+++ game_mechanics/saving.js
@@ -8,6 +8,8 @@ function save() {
   localStorage.setItem("clickMultiplier", JSON.stringify(clickMultiplier));
   localStorage.setItem("grandmaTotal", JSON.stringify(grandmaTotal));
   localStorage.setItem("bakeryTotal", JSON.stringify(bakeryTotal));
+  localStorage.setItem("cowTotal", JSON.stringify(cowTotal));
+  localStorage.setItem("milkTotal", JSON.stringify(milkTotal));
 }
 
 function newGame() {
@@ -16,24 +18,32 @@ function newGame() {
   clickMultiplier = 1
   grandmaTotal = 0
   bakeryTotal = 0
+  cowTotal = 0
+  milkTotal = 0
   localStorage.setItem("cookieTotal", JSON.stringify(cookieTotal));
   localStorage.setItem("clickMultiplier", JSON.stringify(clickMultiplier));
   localStorage.setItem("grandmaTotal", JSON.stringify(grandmaTotal));
   localStorage.setItem("bakeryTotal", JSON.stringify(bakeryTotal));
+  localStorage.setItem("cowTotal", JSON.stringify(cowTotal));
+  localStorage.setItem("milkTotal", JSON.stringify(milkTotal));
   location.reload();
 }
 
 function load() {
-  if (localStorage.getItem("cookieTotal") === null || localStorage.getItem("clickMultiplier") === null || localStorage.getItem("grandmaTotal") === null || localStorage.getItem("bakeryTotal") === null) {
+  if (localStorage.getItem("cookieTotal") === null || localStorage.getItem("clickMultiplier") === null || localStorage.getItem("grandmaTotal") === null || localStorage.getItem("bakeryTotal") === null || localStorage.getItem("cowTotal") === null || localStorage.getItem("milkTotal") === null ) {
     cookieTotal = 0
     clickMultiplier = 1
     grandmaTotal = 0
     bakeryTotal = 0
+    milkTotal = 0
+    cowTotal = 0
   } else {
     cookieTotal = JSON.parse(localStorage.getItem("cookieTotal"));
     clickMultiplier = JSON.parse(localStorage.getItem("clickMultiplier"));
     grandmaTotal = JSON.parse(localStorage.getItem("grandmaTotal"));
     bakeryTotal = JSON.parse(localStorage.getItem("bakeryTotal"));
+    milkTotal = JSON.parse(localStorage.getItem("milkTotal"));
+    cowTotal = JSON.parse(localStorage.getItem("cowTotal"));
   };
 }
 
